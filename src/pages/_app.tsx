4325a03b55a60732fe68549f3c142f94645b39f0
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-09-27 18:27:56 -0300

    initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google"
+import Image from "next/image";
+import logoImg from "../assets/Logo.svg";
+
+import { Container, Header } from "@/styles/pages/app";
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="logo"/>
+      </Header>
+      <Component {...pageProps}/>
+    </Container>
+  )
 }
+
